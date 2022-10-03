576fd45c6ed24a9a5d2f115212b9f9cc9ad2c7c4
David Legendre <legendre2013@hotmail.com>
2022-10-02 19:46:40 -0500

    skeleton

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({isUser}) {
   )
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (context) => {
   try {
     cookie.parse(context.req.headers.cookie);
     return {props: {isUser: true}}
